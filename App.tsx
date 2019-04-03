110a2f2374f4b27bedbc8b51dd685462c9a3a4c4
Yusuke Yagyu <gyugyu@pepabo.com>
2019-04-03 16:51:47 +0900

    stop using debounce

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
-import debounce from 'debounce'
 import React, { Fragment, useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Lint from './Lint'
 
 export default () => {
   const [text, setText] = useState('')
-  const lint = debounce((text: string) => (
-    <Lint text={text} />
-  ), 1000)
   return (
     <Fragment>
       <TextField
