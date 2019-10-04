4b547d1e2f595b4ab5a6d261662d4b885074327f
Yusuke Yagyu <gyugyu@pepabo.com>
2019-10-04 12:33:51 +0900

    run NoMixDearuDesumasu

--- Lint.tsx
+++ Lint.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import NoMixedZenkakuAndHankakuAlphabet from 'textlint-rule-no-mixed-zenkaku-and-hankaku-alphabet'
+import NoMixDearuDesumasu from 'textlint-rule-no-mix-dearu-desumasu'
 import NoTodo from 'textlint-rule-no-todo'
 import { TextLintCore } from 'textlint/src/textlint-core'
 
 interface Props {
   text: string
+}
+
+(window as any).kuromojin = {
+  dicPath: '/dict'
 }
 
 export default ({ text }: Props) => {
@@ -13,7 +18,15 @@ export default ({ text }: Props) => {
     const core = new TextLintCore()
     core.setupRules({
       'no-mixed-zenkaku-and-hankaku-alphabet': NoMixedZenkakuAndHankakuAlphabet,
+      'no-mix-dearu-desumasu': NoMixDearuDesumasu,
       'no-todo': NoTodo,
+    }, {
+      'no-mix-dearu-desumasu': {
+        preferInHeader: '',
+        preferInBody: 'ですます',
+        preferInList: 'である',
+        strict: true
+      }
     })
     core.lintText(text, '.md')
       .then(({ messages }) => {
